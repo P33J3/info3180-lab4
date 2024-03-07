b7f73005a4c459c978086e4d3cbc5f6fb0103761
P33J3 <peterjonlawrence@gmail.com>
2024-03-06 21:23:45 -0500

    completed exercise 4

--- app/views.py
+++ app/views.py
@@ -3,8 +3,10 @@ from app import app, db, login_manager
 from flask import render_template, request, redirect, url_for, flash, session, abort
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.utils import secure_filename
+from werkzeug.security import check_password_hash
 from app.models import UserProfile
 from app.forms import LoginForm
+from is_safe_url import is_safe_url
 
 
 ###
@@ -43,21 +45,37 @@ def login():
 
     # change this to actually validate the entire form submission
     # and not just one field
-    if form.username.data:
+    if form.validate_on_submit():
         # Get the username and password values from the form.
-
+        username = form.username.data
+        password = form.password.data
         # Using your model, query database for a user based on the username
         # and password submitted. Remember you need to compare the password hash.
         # You will need to import the appropriate function to do so.
         # Then store the result of that query to a `user` variable so it can be
         # passed to the login_user() method below.
 
-        # Gets user id, load into session
-        login_user(user)
+        user = db.session.execute(db.select(UserProfile).filter_by(username=username.scalar()))
 
-        # Remember to flash a message to the user
-        return redirect(url_for("home"))  # The user should be redirected to the upload form instead
-    return render_template("login.html", form=form)
+        if user is not None and check_password_hash(user.password, password):
+            # Gets user id, load into session
+            login_user(user)
+
+            # Remember to flash a message to the user
+            flash('Logged in successfully.', 'success')
+
+            next_page = request.args.get('next')
+
+            if not is_safe_url(next_page, request.host):
+                return abort(400)
+
+            return redirect(next_page or url_for('home'))
+
+        else:
+            flash('Username or Password is incorrect.', 'danger')
+
+    flash_errors(form)
+    return redirect(url_for("upload"))  # The user should be redirected to the upload form instead
 
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
